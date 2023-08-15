bd0e8a9a7ccd2a622a14120e2e14e389a5bef74a
Mthunzi Siputuma <131251620+SiputumaMthunzi@users.noreply.github.com>
2023-08-15 16:51:48 +0200

    Revert "deply file"

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import firebase from './firebase';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <>
-    <App firebase={firebase}/>
+    <App />
     </>
 );
 
