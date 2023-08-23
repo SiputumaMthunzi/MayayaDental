55293e2c881fa140d63514f631097ab4e34dfb29
NduduzoGatsheni <nduduzondlovu635@gmail.com>
2023-08-23 12:59:19 +0200

    layout update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -26,7 +26,7 @@ function About() {
         {showSecondText && (
          
          <p>
-            Our clinic is committed to delivering personalized dental solutions that cater to your unique needs. Whether you're seeking general dentistry services, teeth whitening, orthodontics, or restorative treatments, we have you covered.
+            Our clinic is committed to delivering personalized dental solutions that cater to your unique needs. Whether you're seeking general dentistry services, teeth whitening, or restorative treatments, we have you covered.
             Our skilled and caring team uses the latest techniques and technologies to ensure your comfort and satisfaction throughout your dental journey.
             Your oral health is our priority, and we strive to create a positive and stress-free dental experience for every patient.
           </p>
